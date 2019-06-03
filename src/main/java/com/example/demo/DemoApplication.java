948812f7f386e3ed822ed9cd91fff1eb6f17fd37
ShangguanHong <sgh1450280694@gmail.com>
2019-06-03 19:27:01 +0800

    整合mybatis进行连接数据库

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,18 @@
 package com.example.demo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @Author sgh
- * @Date 2019/5/30 20:30
- * @Description: No Description
+ * DemoApplication class
+ *
+ * @author  sgh
+ * @date 2019/5/30 20:30
  */
 
 @SpringBootApplication // 这是启动SpringBoot启动应用程序的入口点
+@MapperScan("com.example.demo.mapper") // 扫描Mapper
 
 public class DemoApplication {
 
---
+++ src/main/java/com/example/demo/controller/CountryController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.mapper.CountryMapper;
+import com.example.demo.model.Country;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * CountryController class
+ *
+ * @author  sgh
+ * @date 2019/5/30 20:30
+ */
+
+@RestController
+
+public class CountryController {
+
+    @Autowired
+    private CountryMapper countryMapper;
+
+    @RequestMapping(value = "/country", method = RequestMethod.GET)
+    public Country findCountryByCode(@RequestParam String code) {
+        Country country = countryMapper.selectByPrimaryKey(code);
+        return country;
+    }
+}
