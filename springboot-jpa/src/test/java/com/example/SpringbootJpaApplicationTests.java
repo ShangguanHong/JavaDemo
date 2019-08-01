1b7c9a7192f57cc9aff4fd77e507c56bdc4f5590
ShangguanHong <sgh1450280694@gmail.com>
2019-08-01 22:30:55 +0800

    整合logback配置

--- springboot-jpa/src/test/java/com/example/SpringbootJpaApplicationTests.java
+++ springboot-jpa/src/test/java/com/example/SpringbootJpaApplicationTests.java
@@ -1,41 +1,45 @@
 package com.example;
 
-import com.example.domain.Role;
-import com.example.domain.User;
-import com.example.repository.RoleRepository;
-import com.example.repository.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootJpaApplicationTests {
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private RoleRepository roleRepository;
-
-    private User user;
-
-    private Role role;
 
     @Test
     public void contextLoads() {
-//        role = roleRepository.findById(2L).get();
-        Role role1 = new Role();
-        role1.setRoleName("test2");
-        role1.setId(3L);
-        user = new User();
-//        user = userRepository.findById(2L).get();
-        user.setId(4L);
-        user.setUserName("test1");
-        user.setUserAddr("beijing");
-        user.getRoles().add(role1);
-        userRepository.save(user);
+        List<String> stringList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            stringList.add("aaaa" + i);
+        }
+        System.out.println("未自定义排序前");
+        System.out.println(stringList);
+        Collections.sort(stringList, (o1, o2) -> {
+            int a = 0;
+            int b = 0;
+            int c = 0;
+            for (int i = 0; i < o1.length(); i++) {
+                a = a + (o1.charAt(i));
+            }
+            for (int i = 0; i < o2.length(); i++) {
+                b = b + (o2.charAt(i));
+            }
+            for (int i = 0; i < "aaaa5".length(); i++) {
+                c = c + "aaaa5".charAt(i);
+            }
+            a = Math.abs(a - c);
+            b = Math.abs(b - c);
+            return a - b;
+        });
+        System.out.println("自定义排序后");
+        System.out.println(stringList);
     }
 
 }
