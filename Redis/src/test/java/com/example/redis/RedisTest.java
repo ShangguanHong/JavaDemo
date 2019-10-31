24a46f3188e188b4bbdd9ddb57ef30a2f92cd1ad
ShangguanHong <sgh1450280694@gmail.com>
2019-10-31 13:04:34 +0800

    refactor: update project

--- Redis/src/test/java/com/example/redis/RedisTest.java
+++ Redis/src/test/java/com/example/redis/RedisTest.java
@@ -3,20 +3,13 @@ package com.example.redis;
 import com.example.redis.domain.User;
 import com.example.redis.service.UserService;
 import com.example.redis.util.RedisUtil;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.corba.se.impl.orbutil.HexOutputStream;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author sgh
